d068dd1318e28427df738c34f2afd94eab3b22cb
DierWey <didier.wey@laposte.net>
2025-05-22 05:53:33 +0200

    Feat (javascript) : Selection des bullets points fonctionnelle ; Projet terminé.

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,9 @@
-const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
-
 const listImage = [
 	"./assets/images/slideshow/slide1.jpg",
 	"./assets/images/slideshow/slide2.jpg",
 	"./assets/images/slideshow/slide3.jpg",
 	"./assets/images/slideshow/slide4.png" 
 ]
-
 const listTagline = [ 
 	"Impressions tous formats <span>en boutique et en ligne</span>",
 	"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
@@ -38,34 +18,42 @@ const nbrSlides = dots.childElementCount;
 let i = 0;
 
 
-function afficherImgsrc(imgsrc) {
-	let bannerImg = document.querySelector('.banner-img')
-	bannerImg.src = imgsrc
+function displayImgsrc(imgsrc) {
+	let bannerImg = document.querySelector('.banner-img');
+	bannerImg.src = imgsrc;
 }
 
-function afficherTagline(tagline) {
-	let zoneTagline = document.querySelector('.zoneTagline')
-	zoneTagline.innerHTML = tagline
+function displayTagline(tagline) {
+	let zoneTagline = document.querySelector('.zoneTagline');
+	zoneTagline.innerHTML = tagline;
 }
 
+function unsetBullet(dotselect) {
+	document.getElementById("dot" + dotselect).classList.remove("dot_selected");
+}
+function setBullet(dotselect) {
+	document.getElementById("dot" + dotselect).classList.add("dot_selected");
+}
 
 arrow_right.addEventListener("click", () => {
+	unsetBullet(i);
 	if(i < nbrSlides - 1)	
 		i++;
 	else
 		i = 0;
-	afficherImgsrc(listImage[i])
-	afficherTagline(listTagline[i])
-    console.log(i)
+	displayImgsrc(listImage[i]);
+	displayTagline(listTagline[i]);
+	setBullet(i);
 });
 arrow_left.addEventListener("click", () => {
+	unsetBullet(i);
 	if(i > 0)	
 		i--;
 	else
 		i = nbrSlides - 1;
-	afficherImgsrc(listImage[i])
-	afficherTagline(listTagline[i])
-    console.log(i)
+	displayImgsrc(listImage[i]);
+	displayTagline(listTagline[i]);
+	setBullet(i);
 });
 
 
